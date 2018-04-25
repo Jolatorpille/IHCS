9c1d1782ecbbf286230ad4cc5c97c7e4e6248141
Matteo Baldi <matteo.baldi@he-arc.ch>
2018-04-25 16:48:09 +0200

    V1 : Equipe & Joueur

--- IHCS/Equipe.h
+++ IHCS/Equipe.h
@@ -1,16 +1,28 @@
 #ifndef EQUIPE_H
 #define EQUIPE_H
+#include <string>
+#include <list>
+#include "Joueur.h"
 
 
 class Equipe
 {
     public:
         Equipe();
+        Equipe(std::string nom, short id);
         virtual ~Equipe();
-
+        //méthodes
+        void Engager(Joueur &j);
+        void Afficher();
+        //attributs
+        std::string nom;
+        std::list<Joueur> palmares;
+        short ID;
     protected:
 
     private:
+        //attributs
+        int niveauEquipe;
 };
 
 #endif // EQUIPE_H
